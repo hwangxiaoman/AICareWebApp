d9c0c6eda5e769b7002099ecb6b85ea0c3e30289
Emzkie The Great <63085286+hwangxiaoman@users.noreply.github.com>
2023-05-12 23:25:59 +0800

    login php changes
    do some minor adjustments

--- php/login.php
+++ php/login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/login.css">
-    <title>Get Started</title>
+    <title>AI Care | Sign in</title>
 </head>
 <body>
     <div class="container">
@@ -14,8 +14,14 @@
             <img src="../assets/login/d2.png" alt="">
         </div>
         <div class="header">
-            <p>Login to your account</p>
+            <p>Login to your Account</p>
         </div>
-
+        <form action="login" method="post">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+            <input type="button" value="Sign in" id="sign-in">
+        </form>
     </div>
 </body>
